57ce2ade47a23489360ec142313642e76977d556
Thomas McDaniel <thomas@Thomass-MacBook-Pro.local>
2020-08-18 16:01:03 -0500

    finished assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,8 +7,12 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(i, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
-
+        
         # TO-DO: swap
         # Your code here
 
@@ -18,7 +22,13 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    swaps = 0
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            arr[i+1], arr[i] = arr[i], arr[i+1]
+            swaps += 1
+    if swaps > 0:
+        return bubble_sort(arr)
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,12 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(len(arr)):
+        # go through the values in the list one at a time
+        # and check if any of them are the target value
+        if arr[i] == target:
+            return i
+    
+    # once the function has run through all the values return -1
     return -1   # not found
 
 
@@ -9,6 +14,30 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    # getting the limitations of the array
+    low =  0
+    high = len(arr) -1
+    mid = 0
+    # making sure that the lowest value is still lower than the highest after
+    # the change in the upper or lower point
+    while low <= high:
+        # set the midpoint to check for the binary search
+        mid = (high + low) // 2
+        # if the target is smaller than the middle value of the array
+        # we change the upper limit to the midpoint - 1 because we know
+        # it isn't the middle value, lets us remove 1 more value each time
+        #  we go through
+        if arr[mid] > target:
+            high = mid -1
+        # if the target is larger than the midpoint, we move the midpoint
+        # to be the low end of our search
+        if arr[mid] < target:
+            low = mid+1
+        # if the midpoint is the target then we return the index of the value
+        if arr[mid] == target:
+            return mid
 
-
-    return -1  # not found
+    # once the low end becomes larger than the high end
+    # we know we have searched all values so return -1
+        # not found
+    return -1
